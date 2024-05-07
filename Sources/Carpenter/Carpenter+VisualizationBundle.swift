8627a1c3b1bd8ee1f509267832651d5fe63a3eb0
TizianoCoroneo <tizianocoroneo@me.com>
2024-05-07 03:21:16 +0200

    fix: restored visualization bundle

--- Sources/Carpenter/Carpenter+VisualizationBundle.swift
+++ Sources/Carpenter/Carpenter+VisualizationBundle.swift
@@ -3,17 +3,26 @@ import SwiftGraph
 
 // MARK: - Visualization helper
 
-//public extension Carpenter {
-//    struct VisualizationBundle: Codable {
-//        public let buildGraph: UnweightedGraph<String>
-//        public let lateInitGraph: UnweightedGraph<String>
-//        public let nodeKinds: [Vertex: AnyFactory.Kind]
-//    }
-//
-//    func exportToVisualizationBundle() -> VisualizationBundle {
-//        .init(
-//            buildGraph: self.dependencyGraph,
-//            lateInitGraph: self.lateInitDependencyGraph,
-//            nodeKinds: self.factoryRegistry.mapValues(\.kind))
-//    }
-//}
+public extension Carpenter {
+    struct VisualizationBundle: Codable {
+        public let buildGraph: UnweightedGraph<String>
+        public let lateInitGraph: UnweightedGraph<String>
+        public let nodeKinds: [String: AnyFactory.Kind]
+    }
+
+    private func representableGraph(
+        _ graph: UnweightedGraph<AnyDependencyKey>
+    ) -> UnweightedGraph<String> {
+        let new = UnweightedGraph<String>(vertices: graph.vertices.map { $0.displayName() })
+        for edge in graph.edgeList() { new.addEdge(edge, directed: edge.directed) }
+        return new
+    }
+
+    func exportToVisualizationBundle() -> VisualizationBundle {
+        .init(
+            buildGraph: representableGraph(dependencyGraph),
+            lateInitGraph: representableGraph(lateInitDependencyGraph),
+            nodeKinds: Dictionary(uniqueKeysWithValues: factoryRegistry
+                .map { ($0.displayName(), $1.kind) }))
+    }
+}
